1a8e86bb4807a81b6441f21c5ab25ba864c7d99d
cyproto <kyashkarande@gmail.com>
2021-07-22 00:18:17 +0530

    Use effects to handle user login.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { EffectsModule } from "@ngrx/effects";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
@@ -9,6 +10,7 @@ import { SharedModule } from "./shared/shared.module";
 import { CoreModule } from "./core.module";
 import { StoreModule } from "@ngrx/store";
 import * as fromAppReducer from "./store/app.reducer";
+import { AuthEffects } from "./auth/store/auth.effects";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -19,6 +21,7 @@ import * as fromAppReducer from "./store/app.reducer";
     SharedModule,
     CoreModule,
     StoreModule.forRoot(fromAppReducer.appReducer),
+    EffectsModule.forRoot([AuthEffects]),
   ],
   bootstrap: [AppComponent],
   // providers: [LoggingService]
--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -2,6 +2,7 @@ import { Action } from "@ngrx/store";
 
 export const LOGIN = "[Auth] Login";
 export const LOGOUT = "[Auth] Logout";
+export const LOGIN_START = "[Auth] Login Start";
 
 export class Login implements Action {
   readonly type = LOGIN;
@@ -20,4 +21,15 @@ export class Logout implements Action {
   readonly type = LOGOUT;
 }
 
-export type AuthActions = Login | Logout;
+export class LoginStart implements Action {
+  readonly type = LOGIN_START;
+
+  constructor(
+    public payload: {
+      email: string;
+      password: string;
+    }
+  ) {}
+}
+
+export type AuthActions = Login | Logout | LoginStart;
