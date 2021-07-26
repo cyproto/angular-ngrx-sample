d760ee7455e49bc6f25b2bdcf49f03319e6d952e
cyproto <kyashkarande@gmail.com>
2021-07-27 01:09:17 +0530

    Utilize SetRecipes and select data from store.

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,27 +1,29 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { map, tap, take, exhaustMap } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { map, tap, take, exhaustMap } from "rxjs/operators";
 
-import { Recipe } from '../recipes/recipe.model';
-import { RecipeService } from '../recipes/recipe.service';
-import { AuthService } from '../auth/auth.service';
+import { Recipe } from "../recipes/recipe.model";
+import { RecipeService } from "../recipes/recipe.service";
+import { Store } from "@ngrx/store";
+import * as fromApp from "../store/app.reducer";
+import * as RecipeActions from "../recipes/store/recipe.actions";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class DataStorageService {
   constructor(
     private http: HttpClient,
     private recipeService: RecipeService,
-    private authService: AuthService
+    private store: Store<fromApp.AppState>
   ) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
     this.http
       .put(
-        'https://angular-ngrx-dfce3-default-rtdb.firebaseio.com/recipes.json',
+        "https://angular-ngrx-dfce3-default-rtdb.firebaseio.com/recipes.json",
         recipes
       )
-      .subscribe(response => {
+      .subscribe((response) => {
         console.log(response);
       });
   }
@@ -29,19 +31,20 @@ export class DataStorageService {
   fetchRecipes() {
     return this.http
       .get<Recipe[]>(
-        'https://angular-ngrx-dfce3-default-rtdb.firebaseio.com/recipes.json'
+        "https://angular-ngrx-dfce3-default-rtdb.firebaseio.com/recipes.json"
       )
       .pipe(
-        map(recipes => {
-          return recipes.map(recipe => {
+        map((recipes) => {
+          return recipes.map((recipe) => {
             return {
               ...recipe,
-              ingredients: recipe.ingredients ? recipe.ingredients : []
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
             };
           });
         }),
-        tap(recipes => {
-          this.recipeService.setRecipes(recipes);
+        tap((recipes) => {
+          //this.recipeService.setRecipes(recipes);
+          this.store.dispatch(new RecipeActions.SetRecipes(recipes));
         })
       );
   }
