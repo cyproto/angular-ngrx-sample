5bdb424ae3ef6b9db39bbb7b7ea6d9e061d01bee
cyproto <kyashkarande@gmail.com>
2021-07-27 00:35:42 +0530

    Add store-devtools package.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { EffectsModule } from "@ngrx/effects";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
@@ -11,6 +12,7 @@ import { CoreModule } from "./core.module";
 import { StoreModule } from "@ngrx/store";
 import * as fromAppReducer from "./store/app.reducer";
 import { AuthEffects } from "./auth/store/auth.effects";
+import { environment } from "src/environments/environment";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -22,6 +24,7 @@ import { AuthEffects } from "./auth/store/auth.effects";
     CoreModule,
     StoreModule.forRoot(fromAppReducer.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
   ],
   bootstrap: [AppComponent],
   // providers: [LoggingService]
